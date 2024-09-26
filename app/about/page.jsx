daffbe629dc137be8760d3184f60abf8aa1971ce
saadafahmed45 <hafijnahid45@gmail.com>
2024-09-27 02:37:09 +0600

    add the some thinkd

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                Innovating the Future of Software
+                Your Web Solution Partner for Success
               </h1>
               <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl">
                 At Softiven, we've been pushing the boundaries of technology
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="container px-6 md:px-16 py-10 mx-auto">
         <div className="items-center lg:flex">
           <div className="w-full lg:w-1/2 ">
-            <div className="lg:max-w-md">
+            <div className="lg:max-w-md space-y-6 ">
               <h1 className="animate-fade-in text-4xl font-bold text-gray-900 dark:text-darkText lg:text-6xl">
                 Innovative Web Development for Modern
                 <span className="text-primary"> Businesses </span>
@@ -18,13 +18,15 @@ const Hero = () => {
                 expert team crafts stunning websites that drive conversions
               </p>
 
-              <Link
-                href={"/contact"}
-                className="w-1/2 px-5 py-2 mt-6 text-sm tracking-wider text-primaryBg uppercase transition-colors duration-300 transform bg-primary rounded-lg lg:w-auto hover:bg-blue-600 focus:outline-none focus:bg-blue-500  dark:bg-blue-700 dark:hover:bg-blue-800"
-              >
-                {" "}
-                Get Started
-              </Link>
+              <div>
+                <Link
+                  href={"/contact"}
+                  className="w-1/2 px-5 py-2 mt-6 text-sm tracking-wider text-primaryBg uppercase transition-colors duration-300 transform bg-primary rounded-lg lg:w-auto hover:bg-blue-600 focus:outline-none focus:bg-blue-500  dark:bg-blue-700 dark:hover:bg-blue-800"
+                >
+                  {" "}
+                  Get Started
+                </Link>
+              </div>
             </div>
           </div>
 
