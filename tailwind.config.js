f53b55f390e9b7af2e6e22c620ad8a1a0c68b493
Asraful Islam <76778254+iasraful@users.noreply.github.com>
2024-10-01 02:10:44 +0600

    fix the header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,29 +9,30 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#3330E4",
+        primary: "#29ABE2",
+        // secondery: "#29ABE2",
         primaryBg: "#fff",
         darkBackground: "#1F2937",
         darkText: "#F3F4F6",
       },
-       keyframes: {
-        'fade-in': {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
-        'fade-in-up': {
-          '0%': { opacity: 0, transform: 'translateY(20px)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
+        "fade-in-up": {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
         },
-        'fade-in-left': {
-          '0%': { opacity: 0, transform: 'translateX(-20px)' },
-          '100%': { opacity: 1, transform: 'translateX(0)' },
+        "fade-in-left": {
+          "0%": { opacity: 0, transform: "translateX(-20px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
         },
       },
       animation: {
-        'fade-in': 'fade-in 1s ease-out',
-        'fade-in-up': 'fade-in-up 1s ease-out',
-        'fade-in-left': 'fade-in-left 1s ease-out',
+        "fade-in": "fade-in 1s ease-out",
+        "fade-in-up": "fade-in-up 1s ease-out",
+        "fade-in-left": "fade-in-left 1s ease-out",
       },
     },
   },
